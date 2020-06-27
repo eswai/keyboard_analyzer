1fbb7d88644709f2a59932e824ad382217aeb08f
eswai <>
2020-06-27 09:29:05 +0200

    kuromoji形態素解析を導入

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Keyboard from './Keyboard.svelte';
 	import mykeyboard from './jis_romaji.json';
+	import kuromoji from './kuromoji/kuromoji.js';
 
 	let text = "My best keyboard.";
 	mykeyboard.rev = 0;
@@ -10,41 +11,67 @@
 		keydic[mk.id] = mk;
 	}
 
+	function kanaToHira(str) {
+    return str.replace(/[\u30a1-\u30f6]/g, function(match) {
+        var chr = match.charCodeAt(0) - 0x60;
+        return String.fromCharCode(chr);
+    });
+	}
+
 	let uncounted = [];
 	let ul = 0;
 	function analyze() {
-		for (let tk of mykeyboard.keys) {
-			tk.count = 0;
-		}
-		for (let i = 0; i < text.length; i++) {
-			// console.log(text.charAt(i));
-			let ch = text.charAt(i);
-			if (ch in mykeyboard.conversion) {
-				let mc = mykeyboard.conversion[ch]
-				// console.log(c);
-				for (let ck of mc.keys) {
-					keydic[ck].count++;
+		let karray = [];
+		kuromoji.builder({
+			dicPath: 'dict'
+		}).build((error, tokenizer) => {
+			const parsed = tokenizer.tokenize(text);
+			console.log(parsed);
+			for (let pa of parsed) {
+				if (pa.reading) {
+					karray.push(kanaToHira(pa.reading));
+				} else {
+					karray.push(kanaToHira(pa.surface_form));
 				}
-				for (let cs of mc.shift) {
-					keydic[cs].count++;
-				}
-			} else {
-				uncounted.push(ch);
 			}
-		}
+			
+			let ktext = karray.join("");
+			console.log(ktext);
 
-		// normalize count
-		let maxv = 0;
-		for (let tk of mykeyboard.keys) {
-			if (maxv < tk.count) maxv = tk.count;
-		}
-		for (let tk of mykeyboard.keys) {
-			tk.value = tk.count / maxv;
-		}
-		mykeyboard.rev++;
-		// console.log(mykeyboard)
-		console.log(uncounted);
-		ul = uncounted.length;
+			for (let tk of mykeyboard.keys) {
+				tk.count = 0;
+			}
+			for (let i = 0; i < ktext.length; i++) {
+				// console.log(ktext.charAt(i));
+				let ch = ktext.charAt(i);
+				if (ch in mykeyboard.conversion) {
+					let mc = mykeyboard.conversion[ch]
+					// console.log(c);
+					for (let ck of mc.keys) {
+						keydic[ck].count++;
+					}
+					for (let cs of mc.shift) {
+						keydic[cs].count++;
+					}
+				} else {
+					uncounted.push(ch);
+				}
+			}
+
+			// normalize count
+			let maxv = 0;
+			for (let tk of mykeyboard.keys) {
+				if (maxv < tk.count) maxv = tk.count;
+			}
+			for (let tk of mykeyboard.keys) {
+				tk.value = tk.count / maxv;
+			}
+			mykeyboard.rev++;
+			// console.log(mykeyboard)
+			console.log(uncounted);
+			ul = uncounted.length;
+		
+		});
 	}
 </script>
 
