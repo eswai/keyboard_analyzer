9e39dee45386ad93f2900ea7e9252a315122087f
eswai <>
2020-07-05 12:35:58 +0200

    ロジック分離

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script>
   import Keyboard from './Keyboard.svelte';
+  import Chart from 'svelte-frappe-charts';
+  import kuromoji from './kuromoji/kuromoji.js';
+  import {kanaToHira, conv_aozora, conv_kana, analyzeKeyboard} from "./analyzer.js";
+
+  // キー配列
   import romaji from './keyboards/jis_romaji.json';
   import naginata from './keyboards/jis_naginata.json';
   import ortho_naginata from './keyboards/ortho_naginata.json';
@@ -7,9 +12,8 @@
   import eucalyn from './keyboards/jis_eucalyn.json';
   import shingeta from './keyboards/jis_shingeta.json';
   import asuka from './keyboards/jis_asuka123.json';
-  import kuromoji from './kuromoji/kuromoji.js';
-  import Chart from 'svelte-frappe-charts';
 
+  // Material UI
   import Textfield from '@smui/textfield';
   import Button, {Label} from '@smui/button';
   import Dialog, {Title, Content, Actions, InitialFocus} from '@smui/dialog';
@@ -19,6 +23,7 @@
   import DataTable, {Head, Body, Row, Cell} from '@smui/data-table';
   import Card from '@smui/card';
 
+  // 入力UI
   const keyboards = {
     "QWERTYローマ字" : romaji,
     "薙刀式": naginata,
@@ -31,159 +36,50 @@
 
   let selected_kb = "QWERTYローマ字";
   let mykeyboard = keyboards[selected_kb];
-
   let text = "人類が増えすぎた人口を宇宙に移民させるようになって、既に半世紀が過ぎていた。地球の周りの巨大な人工都市は人類の第二の故郷となり、人々はそこで子を産み、育て、そして死んでいった。";
-
-  let uncounted = []; // 入力できなかった文字
-  let ul = 0; // 入力できなかった文字数
-  let total_char = 0; // 入力した文字数
-  let total_key = 0; // 打鍵したキー数
-  let total_schar = 0; // 打鍵したキー数、連続シフトを考慮
-  let total_kana = 0; // 入力した文字数（かな）
-  let renzoku_shift = 0;
-  let finger_tandoku= [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  let finger_douji = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  let finger_shifted = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  let total_action = 0;
-  let same_finger = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   let showkb;
-  let finger_chart;
-  let samefinger_chart;
-  let arpeggio_chart;
-  let last_key = ""; // 直前に押したキー
-  let total_arpeggio;
-  let total_alter = 0; // 交互打鍵
   let kana_only = false;
   let aozora = false;
-  let keyseq = "";
-
   let optionDialog;
   let remarkDialog;
   let remark = "";
 
-  let keydic = {};
-  $: for (let mk of mykeyboard.keys) {
-    keydic[mk.id] = mk;
-  }
+  // 出力UI
+  let ul; // 入力できなかった文字数
+  let ntext; // 入力した文字数
+  let nkey; // 打鍵したキー数
+  let nshift; // 打鍵したキー数、連続シフトを考慮
+  let nkana; // 入力した文字数（かな）
+  let nreshift;
+  let naction;
+  let ndouyubi;
+  let narpeggio;
+  let nkougo; // 交互打鍵
 
-  var sum = function(arr) {
-      return arr.reduce(function(prev, current, i, arr) {
-          return prev+current;
-      });
-  };
+  // 出力グラフ
+  let finger_chart;
+  let samefinger_chart;
+  let arpeggio_chart;
 
   function percent(v) {
     return (v * 100).toFixed(1) + '%'
-  }
-
-  function kanaToHira(str) {
-    return str.replace(/[\u30a1-\u30f6]/g, function(match) {
-        let chr = match.charCodeAt(0) - 0x60;
-        return String.fromCharCode(chr);
-    });
-  }
-
-  let shift_key = [];
-  function incCounter(c) {
-    let mc = mykeyboard.conversion[c]
-    // console.log(c);
-    if (mc.type == "sim") {
-      total_action++;
-    } else {
-      total_action += mc.keys.length + mc.shift.length;
-    }
-    if (mc.shift.length > 0) {
-      total_schar++;
-    }
-
-    for (let ck of mc.keys) {
-      if (mc.shift.length > 0) {
-        keydic[ck].shifted++;
-      } else if (mc.type == 'sim' && mc.keys.length > 1) {
-        keydic[ck].douji++;
-      } else {
-        keydic[ck].tandoku++;
-      }
-      
-      keydic[ck].count++;
-      total_key++;
-      keyseq += ck;
-
-      if (ck in keydic && last_key in keydic) {
-        // 同じ指で違うキーを連続して押す
-        if (ck != last_key && keydic[ck].finger == keydic[last_key].finger) {
-          same_finger[keydic[ck].finger]++;
-        }
-        // アルペジオ
-        for (let i = 0; i < mykeyboard.arpeggio.length; i++) {
-          let ar = mykeyboard.arpeggio[i];
-          if ((ar[0] == ck && ar[1] == last_key) || (ar[1] == ck && ar[0] == last_key)) {
-            total_arpeggio[i]++;
-            // console.log(last_key, ck);
-          }
-        }
-        // 交互打鍵
-        if (((keydic[ck].finger < 5 && keydic[last_key].finger >= 5) || (keydic[ck].finger >= 5 && keydic[last_key].finger < 5))) {
-          total_alter++;
-        }
-      }
-      last_key = ck;
-    }
-    if (mc.shift.length == 0) {
-      shift_key = [];
-    } else {
-      for (let cs of mc.shift) {
-        // 連続シフト
-        if (mc.renzsft && shift_key.includes(cs)) {
-          renzoku_shift++;
-        } else {
-          keydic[cs].count++;
-          total_key++;
-          keydic[cs].shifted++;
-        }
-      }
-      shift_key = mc.shift;
-    }
-
   }
 
   function kbchange() {
     remark = keyboards[selected_kb].remark;
   }
 
-  function analyze() {
+  function startAnalsys() {
     mykeyboard = keyboards[selected_kb];
-
     showkb = false;
-    let karray = []; // 変換後のかな文字の配列
-    uncounted = [];
-    total_char = text.length;
-    total_key = 0;
-    total_schar = 0;
-    renzoku_shift = 0;
-    finger_tandoku = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    finger_douji = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    finger_shifted = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    total_action = 0;
-    shift_key = [];
-    same_finger = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    last_key = "";
-    total_arpeggio = new Array(mykeyboard.arpeggio.length);
-    total_arpeggio.fill(0);
-    total_alter = 0;
-    keyseq = "";
-
-    // 全角英数を半角に変換
-    let hantext = text.replace(/[＂-＇＊-＞＠-ｚ]/g, function(s) {
-      return String.fromCharCode(s.charCodeAt(0) - 65248);
-    });
 
     kuromoji.builder({
       dicPath: 'dict' // public/dict
     }).build((error, tokenizer) => {
       // 形態素解析
-      const parsed = tokenizer.tokenize(hantext);
+      const parsed = tokenizer.tokenize(text);
       // console.log(parsed);
+      let karray = []; // 変換後のかな文字の配列
       for (let pa of parsed) {
         if (pa.reading) { // 漢字、カナ
           karray.push(kanaToHira(pa.reading));
@@ -194,76 +90,39 @@
 
       let ktext = karray.join("");
       if (kana_only) {
-        ktext = ktext.replace(/[\u0000-\u2999\u3003-\u3040\u3097-\uff00\uff02-\uff1e\uff20-\uffff]/g, "");
+        ktext = conv_kana(ktext);
       }
       if (aozora) {
-        ktext = ktext.replace(/（.+?）/g, "");
-        ktext = ktext.replace(/《.+?》/g, "");
-        ktext = ktext.replace(/｜/g, "");
+        ktext = conv_aozora(ktext);
       }
 
-      total_kana = ktext.length;
-      console.log(ktext);
+      let r = analyzeKeyboard(ktext, mykeyboard);
 
-      for (let tk of mykeyboard.keys) {
-        tk.count = 0; // 合計
-        tk.tandoku = 0; // 単独
-        tk.douji = 0; // シフトではない同時押し
-        tk.shifted = 0; // シフト入力
-      }
-      for (let i = 0; i < ktext.length; i++) {
-        // console.log(ktext.charAt(i));
-        let ch1 = ktext.charAt(i);
-        let ch2 = ktext.substr(i, 2);
-        let ch3 = ktext.substr(i, 3);
-        if (ch3 in mykeyboard.conversion) {
-          incCounter(ch3);
-          i += 2;
-        } else if (ch2 in mykeyboard.conversion) {
-          incCounter(ch2);
-          i++;
-        } else if (ch1 in mykeyboard.conversion) {
-          incCounter(ch1)
-        } else {
-          uncounted.push(ch1);
-        }
-      }
+      ntext = text.length;
+      nkana = r.nKana;
+      nkey = r.nKey;
+      naction = r.nAction;
+      nshift = r.nShift;
+      nkougo = r.nKougo;
+      nreshift = r.nReShift;
+      narpeggio = r.nArpeggio;
+      ndouyubi = r.nDouyubi;
+      ul = r.nUncounted;
 
-      for (let k of mykeyboard.keys) {
-        finger_tandoku[k.finger] += k.tandoku;
-        finger_douji[k.finger] += k.douji;
-        finger_shifted[k.finger] += k.shifted;
-      }
-
-      console.log(keyseq);
-      // normalize count
-      let maxv = 0;
-      for (let tk of mykeyboard.keys) {
-        if (maxv < tk.count) maxv = tk.count;
-      }
-      for (let tk of mykeyboard.keys) {
-        tk.value = tk.count / maxv;
-      }
-      mykeyboard.rev = Math.random();
-      // console.log(mykeyboard)
-      console.log(uncounted);
-      ul = uncounted.length;
-
-      // console.log(finger_count);
       finger_chart = {
         labels: ['左小', '左薬', '左中', '左人', '左親', '右親', '右人', '右中', '右薬', '右小'],
         datasets: [
           {
             name: "単独",
-            values: finger_tandoku
+            values: r.finger.tandoku
           },
           {
             name: "同時",
-            values: finger_douji
+            values: r.finger.douji
           },
           {
             name: "シフト",
-            values: finger_shifted
+            values: r.finger.shift
           },
         ]
       };
@@ -271,7 +130,7 @@
         labels: ['左小', '左薬', '左中', '左人', '左親', '右親', '右人', '右中', '右薬', '右小'],
         datasets: [
           {
-            values: same_finger
+            values: r.finger.onaji
           }
         ]
       };
@@ -279,7 +138,7 @@
         labels: mykeyboard.arpeggio,
         datasets: [
           {
-            values: total_arpeggio
+            values: r.arpeggio
           }
         ]
       };
@@ -301,7 +160,7 @@
       {/each}
     </Select>
   
-  <Button color="secondary" on:click={analyze} variant="outlined"><Label>分析開始</Label></Button>
+  <Button color="secondary" on:click={startAnalsys} variant="outlined"><Label>分析開始</Label></Button>
 
   <Dialog bind:this={optionDialog} aria-labelledby="option-title" aria-describedby="option-content" >
       <Title id="option-title">分析オプション</Title>
@@ -364,48 +223,48 @@
           <Body>
             <Row>
               <Cell><div class="textfield">総文字数</div></Cell>
-              <Cell><div class="numberfield">{total_char}</div></Cell>
+              <Cell><div class="numberfield">{ntext}</div></Cell>
               <Cell><div class="numberfield"></div></Cell>
               <Cell><div class="textfield">総カナ文字数</div></Cell>
-              <Cell><div class="numberfield">{total_kana}</div></Cell>
+              <Cell><div class="numberfield">{nkana}</div></Cell>
               <Cell><div class="numberfield"></div></Cell>
             </Row>
             <Row>
               <Cell><div class="textfield">総打鍵数</div></Cell>
-              <Cell><div class="numberfield">{total_key}</div></Cell>
+              <Cell><div class="numberfield">{nkey}</div></Cell>
               <Cell><div class="numberfield"></div></Cell>
               <Cell><div class="textfield">総アクション数</div></Cell>
-              <Cell><div class="numberfield">{total_action}</div></Cell>
+              <Cell><div class="numberfield">{naction}</div></Cell>
               <Cell><div class="numberfield"></div></Cell>
             </Row>
             <Row>
               <Cell><div class="textfield">シフト文字数</div></Cell>
-              <Cell><div class="numberfield">{total_schar}</Cell>
-              <Cell><div class="numberfield">{percent(total_schar / total_kana)}</div></Cell>
+              <Cell><div class="numberfield">{nshift}</Cell>
+              <Cell><div class="numberfield">{percent(nshift / nkana)}</div></Cell>
               <Cell><div class="textfield">うち連続シフト数</div></Cell>
-              <Cell><div class="numberfield">{renzoku_shift}</Cell>
-              <Cell><div class="numberfield">{percent(renzoku_shift / total_kana)}</div></Cell>
+              <Cell><div class="numberfield">{nreshift}</Cell>
+              <Cell><div class="numberfield">{percent(nreshift / nkana)}</div></Cell>
             </Row>
             <Row>
               <Cell><div class="textfield">同指連続数</div></Cell>
-              <Cell><div class="numberfield">{sum(same_finger)}</div></Cell>
-              <Cell><div class="numberfield">{percent(sum(same_finger) / total_action)}</div></Cell>
+              <Cell><div class="numberfield">{ndouyubi}</div></Cell>
+              <Cell><div class="numberfield">{percent(ndouyubi / naction)}</div></Cell>
               <Cell><div class="textfield">うち段越え数</div></Cell>
               <Cell><div class="numberfield"></Cell>
               <Cell><div class="numberfield"></div></Cell>
             </Row>
             <Row>
               <Cell><div class="textfield">左右交互打鍵数</div></Cell>
-              <Cell><div class="numberfield">{total_alter}</div></Cell>
-              <Cell><div class="numberfield">{percent(total_alter / total_action)}</div></Cell>
+              <Cell><div class="numberfield">{nkougo}</div></Cell>
+              <Cell><div class="numberfield">{percent(nkougo / naction)}</div></Cell>
               <Cell><div class="textfield">片手連続数の平均</div></Cell>
               <Cell><div class="numberfield"></Cell>
               <Cell><div class="numberfield"></div></Cell>
             </Row>
             <Row>
               <Cell><div class="textfield">アルペジオ数</div></Cell>
-              <Cell><div class="numberfield">{sum(total_arpeggio)}</div></Cell>
-              <Cell><div class="numberfield">{percent(sum(total_arpeggio) / total_action)}</div></Cell>
+              <Cell><div class="numberfield">{narpeggio}</div></Cell>
+              <Cell><div class="numberfield">{percent(narpeggio / naction)}</div></Cell>
               <Cell><div class="textfield">入力できなかった文字数</div></Cell>
               <Cell><div class="numberfield">{ul}</div></Cell>
               <Cell><div class="numberfield"></div></Cell>
---
+++ src/analyzer.js
@@ -0,0 +1,227 @@
+  let keyboard;
+  let text;
+  let keyseq = "";
+  let keydic = {};
+  let uncounted = []; // 入力できなかった文字
+
+  let finger_tandoku= [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let finger_douji = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let finger_shifted = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let finger_onaji = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let arpeggio;
+  
+  let nkey = 0; // 打鍵したキー数
+  let nreshift = 0; // 連続シフト
+  let nkana = 0; // 入力した文字数（かな）
+  let naction = 0;
+  let nkougo = 0; // 交互打鍵
+  let nshift = 0;
+  let ndangoe = 0;
+
+  let last_key = ""; // 直前に押したキー
+  let shift_key = [];
+
+export function analyzeKeyboard(t, kb) {
+
+  keyboard = kb;
+  text = t;
+
+  preprocess();
+  doAnalyze();
+  let result = postprocess();
+  return result;
+}
+
+function preprocess() {
+  // 全角英数を半角に変換
+  text = text.replace(/[＂-＇＊-＞＠-ｚ]/g, function(s) {
+    return String.fromCharCode(s.charCodeAt(0) - 65248);
+  });
+
+  keydic = {};
+  for (let mk of keyboard.keys) {
+    keydic[mk.id] = mk;
+  }
+}
+
+function postprocess() {
+  return {
+    "nUncounted": uncounted.length,
+    "uncounted": uncounted,
+    "nKana": nkana,
+    "nKey": nkey,
+    "nAction": naction,
+    "nShift": nshift,
+    "nReShift": nreshift,
+    "nDouyubi": sum(finger_onaji),
+    "nDangoe": ndangoe,
+    "nKougo": nkougo,
+    "nArpeggio": sum(arpeggio),
+    "keyboard": keyboard,
+    "finger": {
+      "tandoku": finger_tandoku,
+      "douji": finger_douji,
+      "shift": finger_shifted,
+      "onaji": finger_onaji
+    },
+    "arpeggio": arpeggio
+  };
+}
+
+function incCounter(c) {
+  let mc = keyboard.conversion[c]
+  // console.log(c);
+  if (mc.type == "sim") {
+    naction++;
+  } else {
+    naction += mc.keys.length + mc.shift.length;
+  }
+  if (mc.shift.length > 0) {
+    nshift++;
+  }
+
+  for (let ck of mc.keys) {
+    if (mc.shift.length > 0) {
+      keydic[ck].shifted++;
+    } else if (mc.type == 'sim' && mc.keys.length > 1) {
+      keydic[ck].douji++;
+    } else {
+      keydic[ck].tandoku++;
+    }
+    
+    keydic[ck].count++;
+    nkey++;
+    keyseq += ck;
+
+    if (ck in keydic && last_key in keydic) {
+      // 同じ指で違うキーを連続して押す
+      if (ck != last_key && keydic[ck].finger == keydic[last_key].finger) {
+        finger_onaji[keydic[ck].finger]++;
+      }
+      // アルペジオ
+      for (let i = 0; i < keyboard.arpeggio.length; i++) {
+        let ar = keyboard.arpeggio[i];
+        if ((ar[0] == ck && ar[1] == last_key) || (ar[1] == ck && ar[0] == last_key)) {
+          arpeggio[i]++;
+          // console.log(last_key, ck);
+        }
+      }
+      // 交互打鍵
+      if (((keydic[ck].finger < 5 && keydic[last_key].finger >= 5) || (keydic[ck].finger >= 5 && keydic[last_key].finger < 5))) {
+        nkougo++;
+      }
+    }
+    last_key = ck;
+  }
+  if (mc.shift.length == 0) {
+    shift_key = [];
+  } else {
+    for (let cs of mc.shift) {
+      // 連続シフト
+      if (mc.renzsft && shift_key.includes(cs)) {
+        nreshift++;
+      } else {
+        keydic[cs].count++;
+        nkey++;
+        keydic[cs].shifted++;
+      }
+    }
+    shift_key = mc.shift;
+  }
+
+}
+
+
+function doAnalyze() {
+
+  uncounted = [];
+  nkana = text.length;
+  nkey = 0;
+  nshift = 0;
+  nreshift = 0;
+  finger_tandoku = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  finger_douji = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  finger_shifted = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  naction = 0;
+  shift_key = [];
+  finger_onaji = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  last_key = "";
+  arpeggio = new Array(keyboard.arpeggio.length);
+  arpeggio.fill(0);
+  nkougo = 0;
+  keyseq = "";
+
+  nkana = text.length;
+  console.log(text);
+
+  for (let tk of keyboard.keys) {
+    tk.count = 0; // 合計
+    tk.tandoku = 0; // 単独
+    tk.douji = 0; // シフトではない同時押し
+    tk.shifted = 0; // シフト入力
+  }
+  
+  for (let i = 0; i < text.length; i++) {
+    // console.log(text.charAt(i));
+    let ch1 = text.charAt(i);
+    let ch2 = text.substr(i, 2);
+    let ch3 = text.substr(i, 3);
+    if (ch3 in keyboard.conversion) {
+      incCounter(ch3);
+      i += 2;
+    } else if (ch2 in keyboard.conversion) {
+      incCounter(ch2);
+      i++;
+    } else if (ch1 in keyboard.conversion) {
+      incCounter(ch1)
+    } else {
+      uncounted.push(ch1);
+    }
+  }
+
+  for (let k of keyboard.keys) {
+    finger_tandoku[k.finger] += k.tandoku;
+    finger_douji[k.finger] += k.douji;
+    finger_shifted[k.finger] += k.shifted;
+  }
+
+  console.log(keyseq);
+  // normalize count
+  let maxv = 0;
+  for (let tk of keyboard.keys) {
+    if (maxv < tk.count) maxv = tk.count;
+  }
+  for (let tk of keyboard.keys) {
+    tk.value = tk.count / maxv;
+  }
+  keyboard.rev = Math.random();
+  // console.log(keyboard)
+  console.log(uncounted);
+
+  // console.log(finger_count);
+  
+}
+
+export function kanaToHira(str) {
+  return str.replace(/[\u30a1-\u30f6]/g, function(match) {
+    let chr = match.charCodeAt(0) - 0x60;
+    return String.fromCharCode(chr);
+  });
+}
+
+export function conv_aozora(text) {
+  t = text.replace(/（.+?）/g, "");
+  t = t.replace(/《.+?》/g, "");
+  t = t.replace(/｜/g, "");
+  return t;
+}
+
+export function conv_kana(text) {
+  return text.replace(/[\u0000-\u2999\u3003-\u3040\u3097-\uff00\uff02-\uff1e\uff20-\uffff]/g, "");
+}
+
+function sum(arr) {
+  return arr.reduce(function(prev, current, i, arr) {
+    return prev+current;
+  });
+};
